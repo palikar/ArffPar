ce8ed134a7b6b792a633139a7c11128d1a8b1643
Stanislav Arnaudov <stanislav_ts@abv.bg>
2017-10-09 01:46:24 +0200

    Adding ignore file

---
+++ build/modules/MainModule/CMakeFiles/exe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exe.dir/src/main.cpp.o"
+  "../../bin/exe.pdb"
+  "../../bin/exe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
